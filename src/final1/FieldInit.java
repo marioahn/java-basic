15f08c5ff9cf7a6c6bbdabac16f916885f764758
unknown <marioahn@naver.com>
2024-05-04 15:57:57 +0900

    final-finish

--- src/final1/FieldInit.java
+++ src/final1/FieldInit.java
@@ -2,7 +2,9 @@ package final1;
 
 public class FieldInit {
 
+    // static final은 대문자로 쓰는 관례가 있음ㅇㅇ
+    // 또한, 구분은 언더스코어로! python처럼. 다 대문자니까 카멜이 안되지ㅇㅇ
     static final int CONST_VALUE = 10;
-    final int value = 10;
+    final int value = 10; // 이미 할당해서, 생성자 새로 만들어서 할당 안됨ㅇㅇ
 
 }
