e484fc3120f434e08bc7e5a319b825320a1ee51b
marioahn <marioahn@naver.com>
2024-05-06 00:23:19 +0900

    poly

--- src/extends1/super2/ClassB.java
+++ src/extends1/super2/ClassB.java
@@ -7,7 +7,7 @@ public class ClassB extends ClassA {
         System.out.println("ClassB 생성자 a=" + a);
     }
 
-    public ClassB(int a, int b) {
+    public ClassB(int a, int b)     {
         super(); //기본 생성자 생략 가능
         // super()변수에 아무것도 없다는건 classA에 ClassA() {~}이렇게 기본생성자란 뜻이니까
         // super()도 생략 가능한거임ㅇ. super(10,20)이런게 있으면 기본생성자 아님ㅇ
